c58191eb3dbe9ea0f1d915d8c14e4807f115f05d
sangwookWoo <sangwook.woo@tridge.com>
2023-12-25 21:15:17 +0900

    Movies API 전용  엔드포인트 추가

--- movies/urls.py
+++ movies/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from .views import actor_list, movie_list
 
-urlpatterns = [path("movies", movie_list), path("actors", actor_list)]
+urlpatterns = [
+    path("movies", movie_list),
+    path("movies/<int:pk>", movie_detail),
+    path("actors", actor_list),
+]
