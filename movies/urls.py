537a7e427ca810e5fac6771e0ccd98ddfeb3889e
sangwookWoo <sangwook.woo@tridge.com>
2023-12-25 21:53:05 +0900

    Actors detail API 추가

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import actor_list, movie_detail, movie_list
+from .views import actor_detail, actor_list, movie_detail, movie_list
 
 urlpatterns = [
     path("movies", movie_list),
     path("movies/<int:pk>", movie_detail),
     path("actors", actor_list),
+    path("actors/<int:pk>", actor_detail),
 ]
